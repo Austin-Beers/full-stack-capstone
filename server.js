161ea558b82d5d4d53e655e2e87edff4424a4c0b
Austin Beers <beersa6@students.rowan.edu>
2018-01-28 22:35:40 -0500

    working hello world test

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
 const express = require('express');
+// const morgan = require('morgan');
 const app = express();
+
+const usersRouter = require('./usersRouter')
+const postsRouter = require('./postsRouter')
+const placesRouter = require('./placesRouter')
+// app.use(morgan('common'));
+
+
+
 app.use(express.static('public'));
+
+app.get('/',(req, res) =>{
+    res.sendFile(__dirname + '/public/index.html');
+})
 app.listen(process.env.PORT || 8080);
